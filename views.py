c04891add04fbf717332fb358306d72ee666fb0a
Shreya Jolapara <shreya051006@gmail.com>
2026-01-09 17:53:50 +0530

    Update views.py

--- views.py
+++ views.py
@@ -7,7 +7,12 @@ import json
 views = Blueprint('views', __name__)
 
 
-@views.route('/', methods=['GET', 'POST'])
+@views.route('/', methods=['GET'])
+def landing():
+    return render_template("landingpage.html")
+
+
+@views.route('/home', methods=['GET', 'POST'])
 @login_required
 def home():
     if request.method == 'POST': 
