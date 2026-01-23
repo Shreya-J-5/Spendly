a255336793dacf3f7d1d8516578b7671887c2782
Shreya Jolapara <shreya051006@gmail.com>
2026-01-23 23:02:48 +0530

    Update models.py

--- models.py
+++ models.py
@@ -10,9 +10,21 @@ class Note(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
 
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(20), nullable=False)  # 'Income' or 'Expense'
+    description = db.Column(db.String(500))
+    payment_mode = db.Column(db.String(50))
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
     first_name = db.Column(db.String(150))
     notes = db.relationship('Note')
+    expenses = db.relationship('Expense')
