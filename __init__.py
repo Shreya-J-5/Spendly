87408ba24aa0c70aad6a3bd45edb79cd41275d71
Shreya Jolapara <shreya051006@gmail.com>
2026-01-23 23:03:19 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,12 +4,12 @@ from os import path
 from flask_login import LoginManager
 
 db = SQLAlchemy()
-DB_NAME = "database.db"
+DB_NAME = "spendly.db"
 
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
+    app.config['SECRET_KEY'] = 'ayisgdysiasgdasikasjdhaskydgk'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
